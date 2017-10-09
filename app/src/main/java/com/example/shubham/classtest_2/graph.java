cc048f8a165d561144555161d62350a8c17cdbff
shubham negi <shubhamnegi1990@gmail.com>
2017-10-09 17:56:13 +0530

    initial changes

--- app/src/main/java/com/example/shubham/classtest_2/graph.java
+++ app/src/main/java/com/example/shubham/classtest_2/graph.java
@@ -5,9 +5,11 @@ import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
@@ -25,7 +27,16 @@ public class graph extends AppCompatActivity implements SensorEventListener{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_graph2);
+        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        if(sensorManager != null && sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
+            Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        else {
+            Toast.makeText(getApplicationContext(),"Accelerometer", Toast.LENGTH_LONG).show();
+            System.exit(1);
+        }
 
         GraphView graph = (GraphView) findViewById(R.id.graph);
         series = new LineGraphSeries<>();
@@ -41,6 +52,7 @@ public class graph extends AppCompatActivity implements SensorEventListener{
         float y = sensorEvent.values[1];
         float z = sensorEvent.values[2];
         //w[i] = Math.sqrt(x * x + y * y + z * z);
+        //series = new LineGraphSeries<DataPoint>();
         w = Math.sqrt(x * x + y * y + z * z);
         series.appendData(new DataPoint(count,w),true,50);
         //i++;
@@ -48,6 +60,8 @@ public class graph extends AppCompatActivity implements SensorEventListener{
       //  if (i==11)
         //    i=0;
     }
+
+
     public void back(View v) {
         Intent intent = new Intent(graph.this, MainActivity.class);
         startActivity(intent);
